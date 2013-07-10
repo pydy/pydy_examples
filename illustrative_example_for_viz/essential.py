[WIP] Some error in the numerical integration of the problem. run illustrative_example.py to see what is wrong

illustrative_example_for_viz/essential.py
@@ -2,9 +2,10 @@
 #illustrative example
 #It only contains the basic implementations of pydy-viz
 #also it doesnot check for TypeErrors etc.
+import numpy as np
 
 class MeshShape(object):
-    def __init__(self,name,point_list,color='grey',origin=[0,0,0]):
+    def __init__(self, name, point_list, color='grey', origin=[0,0,0]):
         self._name = name
         self._points = point_list
         self._color = color 
@@ -15,7 +16,7 @@ class MeshShape(object):
         return self._name
         
     @name.setter
-    def name(self,new_name):
+    def name(self, new_name):
         self._name = new_name
                         
     @property
@@ -23,7 +24,7 @@ class MeshShape(object):
         return self._points
         
     @points.setter
-    def points(self,new_point_list):
+    def points(self, new_point_list):
         self._points = new_point_list
     
     @property
@@ -31,7 +32,7 @@ class MeshShape(object):
         return self._color
         
     @color.setter
-    def color(self,new_color):
+    def color(self, new_color):
         self._color = new_color
                  
     @property
@@ -39,30 +40,24 @@ class MeshShape(object):
         return self._origin
         
     @color.setter
-    def origin(self,new_origin):
+    def origin(self, new_origin):
         self._origin = new_origin
         
        
 class VisualizationFrame(object):
-    def __init__(self,name,rigidbody,shape=None):
+    def __init__(self, name, rigidbody, shape=None):
         #It is only to be used for rigidbody here, as per the 
         #specific requirements of the illustrative example
         self._name = name
         self._reference_frame = rigidbody.get_frame()
-        self._point = rigidbody.get_masscenter()
+        self._origin = rigidbody.get_masscenter()
         self._shape = shape
-        self._transformation_matrix = \
-                                 [[1, 0, 0, 0], \
-                                  [0, 1, 0, 0], \
-                                  [0, 0, 1, 0], \
-                                  [0, 0, 0, 1]]
+        self._transform = np.eye(4)
         
-    def homogeneous_transformation(self,rframe):
+    def transform(self,rframe):
         rotation_matrix = self._reference_frame.dcm(rframe)
-        for i in range(0,3):
-            for j in range(0,3):
-                self._transformation_matrix[i][j] = rotation_matrix
-        return self._transformation_matrix            
+        self._transform[0:3,0:3] = rotation_matrix
+        return self._transform
 
     def add_simulation_data(self,file_name=None):
         #TODO
@@ -81,19 +76,16 @@ class Scene():
         self._height = height 
         self._width = width 
     
-    @property
-    def vframes(self):
-        return self._child_vframes
-    @vframes.setter    
-    def vframes(self,vframes):
-        for vframe in vframes:
-            self._child_vframes.append(vframe)
+    
+    def add_visualization_frames(self,vframes):
+        for _vframe in vframes:
+            self._child_vframes.append(_vframe)    
+    
+    def add_visualization_frame(self,vframe):
+        self._child_vframes.append(vframe)
     
     
     def generate_json(self):
         #TODO
         pass    
-        
-        
-       
-        
+    

illustrative_example_for_viz/three_link_pendulum.py
@@ -1,7 +1,8 @@
 #For this one we assume RigidBody links
 from sympy import symbols,sympify
 from sympy.physics.mechanics import *
-
+from code_gen import *
+from scipy.integrate import odeint
 #Number of links = 3
 N_links = 3
 
@@ -29,6 +30,7 @@ M = symbols('M:' + str(N_links))
 #For storing Inertia for each link :
 Ixx = symbols('Ixx:'+str(N_links))
 Iyy = symbols('Iyy:'+str(N_links))
+Izz = symbols('Izz:'+str(N_links))
 
 #gravity and time ....
 g, t = symbols('g t')
@@ -92,9 +94,9 @@ points_rigid_body = [P_link1,P_link2,P_link3]
 
 #defining inertia tensors for links
 
-inertia_link1 = inertia(A,Ixx[0],Iyy[0],0)
-inertia_link2 = inertia(B,Ixx[1],Iyy[1],0)
-inertia_link3 = inertia(C,Ixx[2],Iyy[2],0)
+inertia_link1 = inertia(A,Ixx[0],Iyy[0],Izz[0])
+inertia_link2 = inertia(B,Ixx[1],Iyy[1],Izz[1])
+inertia_link3 = inertia(C,Ixx[2],Iyy[2],Izz[2])
 
 #Defining links as Rigid bodies ...
 
@@ -135,22 +137,49 @@ for angle in alpha:
 	q.append(angle)
 for angle in beta:
 	q.append(angle)
-print q
-u = []
 
+u = []
 for vel in alphad:
 	u.append(vel)
 for vel in betad:
 	u.append(vel)
 
-print u		
+
 kane = KanesMethod(I, q_ind=q, u_ind=u, kd_eqs=kinetic_differentials)
 fr, frstar = kane.kanes_equations(forces, total_system)
 
 print fr
 
+#Now we do numerical integration on the Kane object obtained ...
+params = []
+for lengths in l:
+    params.append(lengths)
+    
+for masses in m:
+    params.append(masses)
+    
+for masses in M:    
+    params.append(masses)
 
+for inertias in Ixx:
+    params.append(inertias)    
 
+for inertias in Iyy:
+    params.append(inertias)        
+    
+for inertias in Izz:
+    params.append(inertias)            
+params.append(g)
+
+print params
+
+right_hand_side = numeric_right_hand_side(kane, params)
+
+param_vals = [10, 10, 10, 5, 5, 5, 10, 10, 10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 9.8]
+t = [i*1 for i in range(0,10)]    #Taking 10 time intervals of 0.1 sec
+x0 = [45,45,45,45,45,45,0,0,0,0,0,0]
+numeric_vals = odeint(right_hand_side, x0, t, args=(param_vals,))
+print numeric_vals 
 
 
 